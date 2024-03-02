5c458fd2a0f7af98feca2e7caa61a438b9b16a2e
carls-ponce <cemmanuel298@gmail.com>
2024-03-02 16:55:33 -0600

    primer commit

---
+++ src/main/java/com/springstudy/xml/XmlBasedApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springstudy/xml/XmlBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
